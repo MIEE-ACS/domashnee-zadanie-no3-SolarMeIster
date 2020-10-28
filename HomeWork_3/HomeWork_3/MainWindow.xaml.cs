c7f7cc40f46eb903d7dc30115e3dd97f1477e18b
SolarMeIster <71086378+SolarMeIster@users.noreply.github.com>
2020-10-28 19:45:28 +0300

    Correct DZ_3

--- HomeWork_3/HomeWork_3/MainWindow.xaml.cs
+++ HomeWork_3/HomeWork_3/MainWindow.xaml.cs
@@ -12,28 +12,35 @@ namespace HomeWork_3
         {
             InitializeComponent();
         }
-        void Comparison_with_the_alphabet(int num_of_let, int i, int j , char[] ch, char[] newTextArray, char[] textArray)
+        void Comparison_with_the_alphabet(int num_of_let, int i, int j , char[] ch, char[] newTextArray, char[] textArray, char[] ch_punctuation)
         {
             if (textArray[i] == ch[j])
             {
                 int number_for_encryption = num_of_let - j;
                 newTextArray[i] = ch[number_for_encryption];
             }
+            else if (j <= 4)
+            {
+                if (textArray[i] == ch_punctuation[j])
+                {
+                    newTextArray[i] = textArray[i];
+                }
+            }
         }
-        void Brute_force(bool flag, string alph, char[] ch, char[] textArray, char[] newTextArray, int i, int num_of_let)
+        void Brute_force(bool flag, string alph, char[] ch, char[] textArray, char[] newTextArray, int i, int num_of_let, char[] ch_punctuation)
         {
             if (flag == true)
             {
                 for (int j = 0; j < alph.Length; j++)
                 {
-                    Comparison_with_the_alphabet(num_of_let, i, j, ch, newTextArray, textArray);
+                    Comparison_with_the_alphabet(num_of_let, i, j, ch, newTextArray, textArray, ch_punctuation);
                 }
             }
             else
             {
                 for (int j = (alph.Length) - 1; j >= 0; j--)
                 {
-                    Comparison_with_the_alphabet(num_of_let, i, j, ch, newTextArray, textArray);
+                    Comparison_with_the_alphabet(num_of_let, i, j, ch, newTextArray, textArray, ch_punctuation);
                 }
             }
 
@@ -56,6 +63,8 @@ namespace HomeWork_3
             if (canConvert == false)
             {
                 bool checkLanguage = false;
+                bool check_registor_rus = false;
+                bool check_registor_eng = false;
                 tb_for_decryption.Visibility = Visibility.Visible;
                 bt_decryption.Visibility = Visibility.Visible;
                 l_decryption.Visibility = Visibility.Visible;
@@ -63,26 +72,44 @@ namespace HomeWork_3
                 tb_result.Visibility = Visibility.Visible;
                 string alph_rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
                 string alph_eng = "abcdefghijklmnoprstuvwxyz";
+                string high_registor_rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+                string high_registor_eng = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+                string punctuation = ",. !?";
                 var ch_eng = alph_eng.ToArray();
                 var ch_rus = alph_rus.ToArray();
+                var ch_high_registor_rus = high_registor_rus.ToArray();
+                var ch_high_registor_eng = high_registor_eng.ToArray();
+                var ch_punctuation = punctuation.ToArray();
                 var textArray = text.ToArray();
                 char first_letter = textArray[0];
                 char[] newTextArray = new char[textArray.Length];
                 for (int i = 0; i < alph_rus.Length; i++)
-                {
-                    if (first_letter == ch_rus[i])
-                    {
+                    if (first_letter == ch_rus[i] || first_letter == ch_high_registor_rus[i])
                         checkLanguage = true;
-                    }
-                }
+                for (char c = 'A'; c <= 'Z'; c++)
+                    if (first_letter == c)
+                        check_registor_eng = true;
+                for (char c = 'А'; c<= 'Я'; c++)
+                    if(first_letter == c)
+                        check_registor_rus = true;
                 int eng = 24;
                 int rus = 32;
                 for (int i = 0; i < text.Length; i++)
                 {
-                    if (checkLanguage == true)
-                        Brute_force(flag, alph_rus, ch_rus, textArray, newTextArray, i, rus);
-                    else
-                        Brute_force(flag, alph_eng, ch_eng, textArray, newTextArray, i, eng);
+                    if (checkLanguage == true && check_registor_rus == false)
+                        Brute_force(flag, alph_rus, ch_rus, textArray, newTextArray, i, rus, ch_punctuation);
+                    else if (checkLanguage == true && check_registor_rus == true)
+                    {
+                        Brute_force(flag, high_registor_rus, ch_high_registor_rus, textArray, newTextArray, i, rus, ch_punctuation);
+                        check_registor_rus = false;
+                    }
+                    else if (checkLanguage == false && check_registor_eng == false)
+                        Brute_force(flag, alph_eng, ch_eng, textArray, newTextArray, i, eng, ch_punctuation);
+                    else if (checkLanguage == false && check_registor_eng == true)
+                    {
+                        Brute_force(flag, high_registor_eng, ch_high_registor_eng, textArray, newTextArray, i, eng, ch_punctuation);
+                        check_registor_eng = false;
+                    }
                 }
                 string newText = "";
                 for (int i = 0; i < text.Length; i++)
